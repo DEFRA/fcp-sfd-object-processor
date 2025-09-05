c5e281a8b142cf8c4ac1c17cd646cc37b7a7874a
Simon Dunn <simondunn90@gmail.com>
2025-09-05 12:13:52 +0100

    happy path integration tests

--- test/integration/narrow/api/initiate.test.js
+++ test/integration/narrow/api/initiate.test.js
@@ -1,46 +1,72 @@
+import { constants as httpConstants } from 'node:http2'
+
 import { describe, test, expect, afterAll, beforeAll } from 'vitest'
 import { createServer } from '../../../../src/api'
 
 describe('/initiate route', () => {
   let server
-  let response
+  let validResponse
 
   beforeAll(async () => {
     server = await createServer()
-    server.initialize()
+    await server.initialize()
 
-    response = await server.inject({
+    validResponse = await server.inject({
       method: 'POST',
       url: '/initiate',
       payload: {
-        test: 123
+        redirect: 'https://myservice.com/redirect',
+        callback: 'https://myservice.com/callback',
+        s3Bucket: 'fcp-sfd-object-processor-bucket',
+        s3Path: 'scanned',
+        metadata: {
+          customerId: '1234',
+          accountId: '1234'
+        }
       }
     })
   })
 
   afterAll(async () => {
-    // server.stop()
+    await server.stop()
   })
 
   describe('/health endpoint for cdp uploader', () => {
     test('returns 200 status', async () => {
       const healthResponse = await fetch('http://cdp-uploader:7337/health')
-      expect(healthResponse.status).toBe(200)
+      expect(healthResponse.status).toBe(httpConstants.HTTP_STATUS_OK)
     })
   })
 
   describe('POST with a valid payload', () => {
-    test('returns 201 status', () => {
-      expect(response.statusCode).toBe(201)
+    test('should return 201 status', () => {
+      expect(validResponse.statusCode).toBe(httpConstants.HTTP_STATUS_CREATED)
     })
 
     test('should return the uploadId, uploadUrl and statusUrl', () => {
+      const jsonResponse = JSON.parse(validResponse.payload)
       const expectedResponse = {
         uploadId: expect.any(String),
         uploadUrl: expect.any(String),
         statusUrl: expect.any(String)
       }
-      expect(response.body).toMatchObject(expectedResponse)
+
+      expect(jsonResponse).toMatchObject(expectedResponse)
+    })
+
+    test('should return uploadId as a valid uuid4', () => {
+      const uuidV4Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+      const jsonResponse = JSON.parse(validResponse.payload)
+
+      expect(jsonResponse.uploadId).toMatch(uuidV4Regex)
+    })
+
+    test('should return uploadUrl and statusUrl with correct uploadId', () => {
+      const baseUrl = 'http://localhost:7337'
+      const jsonResponse = JSON.parse(validResponse.payload)
+
+      expect(jsonResponse.uploadUrl).toMatch(`${baseUrl}/upload-and-scan/${jsonResponse.uploadId}`)
+      expect(jsonResponse.statusUrl).toMatch(`${baseUrl}/status/${jsonResponse.uploadId}`)
     })
   })
 })
