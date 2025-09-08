e769205416cb53cfc41dac8518e11184a6aee198
Simon Dunn <simondunn90@gmail.com>
2025-09-08 15:40:00 +0100

    db config

---
+++ src/config/database.js
@@ -0,0 +1,23 @@
+export const databaseConfig = {
+  mongo: {
+    uri: {
+      doc: 'URI for mongodb',
+      format: String,
+      default: 'mongodb://127.0.0.1:27017/',
+      env: 'MONGO_URI'
+    },
+    database: {
+      doc: 'database for mongodb',
+      format: String,
+      default: 'fcp-sfd-object-processor',
+      env: 'MONGO_DATABASE'
+    },
+    collections: {
+      uploadMetadata: {
+        doc: 'uploadMetadata collection',
+        format: String,
+        default: 'uploadMetadata'
+      }
+    }
+  }
+}
