dd101855f62f2ccc2b531de7f22bfdd910bcfe9b
Simon Dunn <simondunn90@gmail.com>
2025-10-06 14:42:16 +0100

    add validation and unhappy path tests

--- src/api/v1/blobs/index.js
+++ src/api/v1/blobs/index.js
@@ -2,6 +2,7 @@ import Boom from '@hapi/boom'
 import { constants as httpConstants } from 'node:http2'
 
 import { getS3ReferenceByFileId } from '../../../repos/metadata.js'
+import { blobParamSchema } from './schemas/params.js'
 import { generatePresignedUrl } from '../../../repos/s3.js'
 import { NotFoundError } from '../../../errors/not-found-error.js'
 import { config } from '../../../config/index.js'
@@ -13,12 +14,12 @@ export const blobRoute = {
   path: `${baseUrl}/blob/{fileId}`,
   options: {
     tags: ['api', 'blob'],
-    // validate: {
-    //   params: 'metadataParamSchema',
-    //   failAction: (_request, _h, err) => {
-    //     throw err
-    //   },
-    // },
+    validate: {
+      params: blobParamSchema,
+      failAction: (_request, _h, err) => {
+        throw err
+      },
+    },
     // response: {
     //   status: responseSchemas
     // }
--- test/integration/narrow/api/blob.test.js
+++ test/integration/narrow/api/blob.test.js
@@ -1,5 +1,5 @@
 import { constants as httpConstants } from 'node:http2'
-import { vi, describe, test, expect, beforeAll, afterAll } from 'vitest'
+import { vi, describe, test, expect, beforeAll, beforeEach, afterAll } from 'vitest'
 
 import db from '../../../../src/data/db.js'
 import { config } from '../../../../src/config'
@@ -16,6 +16,9 @@ beforeAll(async () => {
   originalCollection = config.get('mongo.collections.uploadMetadata')
   config.set('mongo.collections.uploadMetadata', 'blob-test-collection')
   collection = config.get('mongo.collections.uploadMetadata')
+})
+
+beforeEach(async () => {
   await db.collection(collection).deleteMany({})
 })
 
@@ -43,56 +46,52 @@ describe('GET to the /api/v1/blob/{fileId} route', async () => {
       expect(response.statusCode).toBe(httpConstants.HTTP_STATUS_OK)
       expect(response.result.data.url).toBeDefined()
       expect(response.result.data.url).toContain(`${mockFormattedMetadata.s3.bucket}/${mockFormattedMetadata.s3.key}`)
-      // presignedUrl should be ${bucket}/${key} ...
+    })
+
+    test('should return 404 when no records found', async () => {
+      const response = await server.inject({
+        method: 'GET',
+        url: `/api/v1/blob/${fileId}`,
+      })
+
+      expect(response.statusCode).toBe(httpConstants.HTTP_STATUS_NOT_FOUND)
+      expect(response.result.error).toBe('Not Found')
+      expect(response.result.message).toBe('No documents found')
     })
   })
 
-  // describe('with an invalid fileId', async () => {
-  //   test('should fail validation', async () => {
-  //     const response = await server.inject({
-  //       method: 'POST',
-  //       url: '/api/v1/callback',
-  //       payload: {}
-  //     })
+  describe('with an invalid fileId', async () => {
+    test('should return bad request status when fileid is invalid', async () => {
+      const fileId = 'invalid-fileId'
+      await db.collection(collection).insertOne(mockFormattedMetadata)
 
-  //     const mappedErrors = response.result.err.details.map((detail) => detail.message)
+      const response = await server.inject({
+        method: 'GET',
+        url: `/api/v1/blob/${fileId}`,
+      })
 
-  //     expect(response.statusCode).toBe(httpConstants.HTTP_STATUS_BAD_REQUEST)
+      expect(response.statusCode).toBe(httpConstants.HTTP_STATUS_BAD_REQUEST)
+      expect(response.result.error).toBe('Bad Request')
+      expect(response.result.message).toBe('The "id" field must be a valid UUID v4.')
+    })
+  })
 
-  //     expect(mappedErrors[0]).toBe('"uploadStatus" is required')
-  //     expect(mappedErrors[1]).toBe('"metadata" is required')
-  //     expect(mappedErrors[2]).toBe('"form" is required')
-  //   })
-  // })
+  describe('When the db is unavailable', () => {
+    test('should return 500 server error', async () => {
+      await db.client.close()
 
-  // describe('when the database fails to store the document', async () => {
-  //   test('should return a 500 status code and insert error message', async () => {
-  //     const mockInsertOne = vi.fn().mockResolvedValue({ acknowledged: false })
-  //     vi.spyOn(db, 'collection').mockReturnValue({ insertOne: mockInsertOne })
+      const errorServer = await createServer()
+      const fileId = mockFormattedMetadata.file.fileId
+      const response = await errorServer.inject({
+        method: 'GET',
+        url: `/api/v1/blob/${fileId}`,
+      })
 
-  //     const response = await server.inject({
-  //       method: 'POST',
-  //       url: '/api/v1/callback',
-  //       payload: mockScanAndUploadResponse
-  //     })
-  //     expect(response.statusCode).toBe(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
-  //     expect(response.result.message).toBe('An internal server error occurred')
-  //   })
-  // })
+      expect(response.result.statusCode).toBe(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
+      expect(response.result.error).toBe('Internal Server Error')
+      expect(response.result.message).toBe('An internal server error occurred')
 
-  // describe('when the database is unavailable', async () => {
-  //   test('should return a 500 status code and database unavailable message', async () => {
-  //     const dbError = vi.fn().mockRejectedValue(new Error('Database unavailable'))
-  //     vi.spyOn(db, 'collection').mockReturnValue({ insertOne: dbError })
-
-  //     const response = await server.inject({
-  //       method: 'POST',
-  //       url: '/api/v1/callback',
-  //       payload: mockScanAndUploadResponse
-  //     })
-
-  //     expect(response.statusCode).toBe(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
-  //     expect(response.result.error).toBe('Internal Server Error')
-  //     expect(response.result.message).toBe('An internal server error occurred')
-  //   })
+      await db.client.connect() // reconnect to allow test clean up
+    })
+  })
 })
