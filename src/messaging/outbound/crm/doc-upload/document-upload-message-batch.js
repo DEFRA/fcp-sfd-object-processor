5fa394993b75e271fdd57785b82d2f07a3150c2e
Simon Dunn <simondunn90@gmail.com>
2026-01-16 12:18:59 +0000

    refactor: change to building batches with array of messages

---
+++ src/messaging/outbound/crm/doc-upload/document-upload-message-batch.js
@@ -0,0 +1,15 @@
+export const buildDocumentUploadMessageBatch = (pendingMessages) => {
+  return pendingMessages.map(payload => {
+    return {
+      id: crypto.randomUUID(),
+      source: 'fcp-sfd-object-processor',
+      specversion: '1.0',
+      type: 'uk.gov.fcp.sfd.document.upload.case.create', // placeholder until we know the real contract
+      datacontenttype: 'application/json',
+      time: new Date().toISOString(),
+      data: {
+        ...payload
+      } // placeholder until we know the real contract
+    }
+  })
+}
