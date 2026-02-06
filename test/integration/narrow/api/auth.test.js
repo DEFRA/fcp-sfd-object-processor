c72d55e8a8a99d10272dfd54c4d87b2bd7770d33
Simon Dunn <simondunn90@gmail.com>
2026-02-06 11:13:25 +0000

    feat: setup auth tests

---
+++ test/integration/narrow/api/auth.test.js
@@ -0,0 +1,52 @@
+import { constants as httpConstants } from 'node:http2'
+import { vi, describe, test, expect, beforeAll, beforeEach, afterAll } from 'vitest'
+
+import { db } from '../../../../src/data/db.js'
+import { config } from '../../../../src/config'
+import { createServer } from '../../../../src/api'
+import { mockFormattedMetadata } from '../../../mocks/metadata.js'
+
+let server
+let originalCollection
+let collection
+
+beforeAll(async () => {
+  // set a new collection for each integration test to avoid db clashes between tests
+  vi.restoreAllMocks()
+  originalCollection = config.get('mongo.collections.uploadMetadata')
+  config.set('mongo.collections.uploadMetadata', 'auth-test-collection')
+  collection = config.get('mongo.collections.uploadMetadata')
+
+  server = await createServer()
+  await server.initialize()
+
+  await db.collection(collection).insertOne(mockFormattedMetadata)
+})
+
+describe('Authentication across all routes', () => {
+  describe('Protected routes must require auth: /blobs, /metadata', () => {
+    const routes = [
+      { method: 'GET', url: `/api/v1/blob/${mockFormattedMetadata.file.fileId}` },
+      { method: 'GET', url: `/api/v1/metadata/sbi/${mockFormattedMetadata.metadata.sbi}` }
+    ]
+
+    test.each(routes)('$method $url returns 401 when no auth header is present', async ({ method, url }) => {
+      const response = await server.inject({
+        method,
+        url
+      })
+
+      expect(response.statusCode).toBe(httpConstants.HTTP_STATUS_UNAUTHORIZED)
+      expect(response.result.error).toBe('Unauthorized')
+      expect(response.result.message).toBe('Missing authentication')
+    })
+
+    // test.each(routes)('$method $url returns 403 when auth token is present but is wrong security group. ', async ({ method, url }) => {
+    // })
+
+    // returns 200 when auth is enabled and valid token provided
+  })
+})
+
+// describe('and the auth token is valid', () => {
+// })
